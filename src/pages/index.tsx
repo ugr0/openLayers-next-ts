1d275197246538d4112733eee14b64637e1e8c09
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-14 11:48:07 +0900

    🔥 複雑なmapコンポーネントを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from "next";
-import { Map } from "src/components/map";
 import MapPane from "src/components/MapPane";
 
 const Home: NextPage = () => {
   return (
     <>
-      {/* <Map></Map> */}
       <MapPane />
     </>
   );
