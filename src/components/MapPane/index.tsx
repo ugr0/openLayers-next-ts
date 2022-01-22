5e20969dbd77838a851fcb18431ae1ad24b3cdb3
uglo1 <yujiro.takahashi0912@gmail.com>
2022-01-23 06:47:46 +0900

    ⚗️ weightプロパティで色味の変化を付けた

--- src/components/MapPane/index.tsx
+++ src/components/MapPane/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, VFC } from "react";
+import { useCallback, useEffect, useRef, useState, VFC } from "react";
 
 // OpenLayers読み込み
 import Map from "ol/Map";
@@ -13,6 +13,8 @@ import Feature from "ol/Feature";
 import Point from "ol/geom/Point";
 
 import classes from "./MapPane.module.css";
+import MapData from "src/data/mapData.json";
+import { Slider } from "@mui/material";
 
 type Props = {
   lon: number;
@@ -24,46 +26,98 @@ type Props = {
 
 export const MapPane: VFC<Props> = (props) => {
   const { lon, lat, zoom, width, height } = props;
+  const [blur, setBlur] = useState(20);
+  const [radius, setRadius] = useState(20);
   let container: any = useRef(null);
 
-  // TODO イマイチ
-  const styles = {
-    width: width,
-    height: height,
-  };
+  // ヒートマップのblurハンドラ
+  const handleChangeBlur = useCallback(
+    (event: Event, newValue: number | number[]) => {
+      setBlur(newValue as number);
+    },
+    []
+  );
+
+  // ヒートマップのradiusハンドラ
+  const handleChangeRadius = useCallback(
+    (event: Event, newValue: number | number[]) => {
+      setRadius(newValue as number);
+    },
+    []
+  );
+
+  // OpenStreetMap Layer
+  const osm = new TileLayer({
+    source: new OSM(),
+  });
+
+  // 地域ごとの緯度経度、値を取得
+  let featureData: Feature<Point>[] = [];
+  Object.values(MapData).map((data) => {
+    const feature = new Feature({
+      geometry: new Point(fromLonLat([data.lon, data.lat])),
+      value: data.value,
+    });
+    featureData = [...featureData, feature];
+  });
+
+  // HeatMapLayer
+  const hml = process.browser
+    ? new HeatmapLayer({
+        source: new VectorSource({
+          features: featureData,
+        }),
+        blur: 15,
+        radius: 20,
+        weight: (feature) => {
+          return feature.get("value");
+        },
+      })
+    : undefined;
 
   useEffect(() => {
-    const osm = new TileLayer({
-      source: new OSM(),
-    });
-
-    const hml = new HeatmapLayer({
-      source: new VectorSource({
-        features: [
-          new Feature({
-            geometry: new Point(fromLonLat([130.853, 33.571])),
+    const map = process.browser
+      ? new Map({
+          target: container,
+          layers: [osm],
+          view: new View({
+            center: fromLonLat([lon, lat]),
+            zoom,
+            maxZoom: 14,
+            minZoom: 8,
           }),
-          new Feature({
-            geometry: new Point(fromLonLat([130.686, 33.635])),
-          }),
-        ],
-      }),
-      blur: 50,
-      radius: 40,
-    });
-
-    const map = new Map({
-      target: container,
-      layers: [osm, hml],
-      view: new View({
-        center: fromLonLat([lon, lat]),
-        zoom,
-      }),
-    });
-    map.setSize([width, height]);
-  }, [lon, lat, zoom, width, height]);
+        })
+      : null;
+    map?.addLayer(hml as HeatmapLayer);
+  }, []);
 
   return (
-    <div className={classes.map} style={styles} ref={(e) => (container = e)} />
+    <>
+      <div
+        className={classes.map}
+        style={{ width, height }}
+        ref={(e) => (container = e)}
+      />
+
+      <h1>半径とぼやけ調整(TODO)</h1>
+      <p>blur: {blur}</p>
+      <Slider
+        aria-label="blur"
+        valueLabelDisplay="auto"
+        value={blur}
+        onChange={handleChangeBlur}
+        min={1}
+        max={50}
+      />
+      <p>radius: {radius}</p>
+      <Slider
+        aria-label="radius"
+        valueLabelDisplay="auto"
+        value={radius}
+        onChange={handleChangeRadius}
+        min={1}
+        max={50}
+      />
+    </>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,17 @@ import type { NextPage } from "next";
 import { MapPane } from "src/components/MapPane";
 import MapData from "../data/mapData.json";
 
+import styles from "src/styles/Home.module.css";
+
 const Home: NextPage = () => {
   return (
-    <>
-      <MapPane {...MapData.soeda} zoom={10} width={800} height={600} />
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>Open Layers Demo</h1>
+        <MapPane {...MapData.soeda} zoom={10} width={800} height={600} />
+      </div>
       <br />
-      <MapPane {...MapData.iizuka} zoom={14} width={500} height={500} />
-    </>
+    </div>
   );
 };
 
